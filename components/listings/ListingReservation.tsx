10f828291b8fff1be27abf26b9fc500da6423a08
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-11-12 12:48:46 +0530

    date range ui

---
+++ components/listings/ListingReservation.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Button from "@components/Button";
+import Calendar from "@components/inputs/Calendar";
+import { Range } from "react-date-range";
+
+interface ListingReservationProps {
+	price: number;
+	dateRange: Range;
+	totalPrice: number;
+	onChangeDate: (value: Range) => void;
+	onSubmit: () => void;
+	disabled?: boolean;
+	disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+	price,
+	dateRange,
+	totalPrice,
+	onChangeDate,
+	onSubmit,
+	disabledDates,
+	disabled,
+}) => {
+	return (
+		<div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+			<div className="flex flex-row items-center gap-1 p-4">
+				<h4 className="text-2xl font-semibold">$ {price}</h4>
+				<p className="font-light text-neutral-600">night</p>
+			</div>
+			<hr />
+			<Calendar
+				value={dateRange}
+				disabledDates={disabledDates}
+				onChange={(value) => onChangeDate(value.selection)}
+			/>
+			<hr />
+            <div className="p-4">
+                <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
+            </div>
+			<div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+				<p>Total</p>
+				<p>$ {totalPrice}</p>
+			</div>
+		</div>
+	);
+};
+
+export default ListingReservation;
