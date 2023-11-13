b3960ed733e9f8d150d7d43a8e0c7e74f532005c
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-11-13 21:53:38 +0530

    trip cancel add

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Airbnb Clone using `Nextjs`, `Typescript`,`TailwindCSS`,`Prisma`, `MongoDB`, `NextAuth` and `Zustand` for react state management.
 
 
+## Screenshot
+![Homepage](./homepage.png)
+
---
+++ app/reservations/page.tsx
@@ -0,0 +1,12 @@
+import EmptyState from "@components/EmptyState"
+
+import getCurrentUser from "@actions/getCurrentUser"
+import getReservations from "@actions/getReservations"
+
+const ReservationsPage = () => {
+  return (
+    <div>ReservationsPage</div>
+  )
+}
+
+export default ReservationsPage
