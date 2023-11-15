77cb8cb59c6dd11c57d466c15618cc3e1a9dcea2
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-11-15 19:27:55 +0530

    favorites lising add

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -38,7 +38,9 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success('Successfully created new account!')
         registerModal.onClose();
+        loginModal.onOpen()
       })
       .catch((error) => {
         toast.error("Something went wrong");
